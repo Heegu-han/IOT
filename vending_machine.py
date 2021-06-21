117a1da1825dfc36a54031ff39d23dcd1d89bd5c
Heegu-han <hanheegu7@gmail.com>
2021-06-21 06:29:03 +0000

    vending

--- vending_machine.py
+++ vending_machine.py
@@ -5,18 +5,18 @@ while 1:
     if number==1:
         print ("포도주스를 선택하셨습니다. 거스름돈은 ",money-100,"원 입니다")
         money=temp-100
-	if money<=0:
-		break
+        if money<=0:
+            break
     elif number==2:
-	print ("오렌지주스를 선택하셨습니다. 거스름돈은 ",money-200,"원 입니다")
-	money=temp-200
-	if money<=0:
-		break
+    print ("오렌지주스를 선택하셨습니다. 거스름돈은 ",money-200,"원 입니다")
+    money=temp-200
+    if money<=0:
+        break
 
     elif number==3:
-	print ("환타를 선택하셨습니다. 거스름돈은 ",money-300,"원 입니다")
-	money=temp-300
-	if money<=0:
-		break
+    print ("환타를 선택하셨습니다. 거스름돈은 ",money-300,"원 입니다")
+    money=temp-300
+    if money<=0:
+        break
     else:
-	print ("없는 번호입니다. 다시 입력해주세요")
+    print ("없는 번호입니다. 다시 입력해주세요")
